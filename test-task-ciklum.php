e6b629b04f2f520b631ba4a7f4799eeb3196efd4
sergiofollean <sergiofollean@gmail.com>
2023-05-02 17:04:46 +0300

    Done

--- test-task-ciklum.php
+++ test-task-ciklum.php
@@ -1,17 +1,47 @@
 <?php
 /*
  * Plugin Name: Test Task Ciklum
+ * Description: allows you to search post/pages/post_types by slug inside /wp-admin area
+ * Version: 1.0
+ * Author: Popovych Sergiy
+ * Author URI: https://www.linkedin.com/in/sergiofollean/
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
+    exit;
 }
 
-// check if class exist TestTaskCiklum
 if ( !class_exists('TestTaskCiklum') ) {
     class TestTaskCiklum {
         public function __construct() {
-            // add_action( 'init', array( $this, 'init' ) );
+            add_filter( 'posts_search', array( $this, 'search_by_slug' ) );
+        }
+
+        public function search_by_slug( $search ) {
+            global $wpdb, $wp_query;
+
+            if ( empty( $search ) )
+                return $search;
+
+            // check if is admin or ajax request
+            if (is_admin()) {
+                $s = $wp_query->query_vars['s'] ?? $_POST['q'] ?? $_POST['s'] ?? '';
+
+                // check if search string have a "slug:" prefix
+                if (strpos($s, 'slug:') === 0) {
+                    $slug = explode(':', $s)[1];
+
+                    $search = $wpdb->prepare(
+                        " AND {$wpdb->posts}.post_name LIKE %s ",
+                        '%' . $wpdb->esc_like( $slug ) . '%'
+                    );
+
+                    $wp_query->set('orderby', 'post_name' );
+                    $wp_query->set('order', 'ASC' );
+                }
+            }
+
+            return $search;
         }
     }
 
